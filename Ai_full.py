25ee2cec730e6672fe429347c96028d6bc40fd61
Andrey Kuznetsov <andr.kuzn10@gmail.com>
2024-04-10 18:52:03 +0300

    I mast a shoot

--- Ai_full.py
+++ Ai_full.py
@@ -3,52 +3,77 @@ import pygame
 
 from settings import Settings
 from ship import Ship
+from bullet import Bullet
 
 class AlienInvasion:#класс для управления кода
 
     def __init__(self):# создаем игровые ресурсы
 
-        pygame.init()#почему не ставится нижние подчеркивание
+        pygame.init()
         self.settings = Settings()
-
-        self.screen = pygame.display.set_mode((self.settings.screen_width, self.settings.screen_height))  
+        
+        self.screen = pygame.display.set_mode((0, 0), pygame.FULLSCREEN)
+        self.settings.screen_height = self.screen.get_rect().height#высота
+        self.settings.screen_width = self.screen.get_rect().width#ширина
+        #self.screen = pygame.display.set_mode((self.settings.screen_width, self.settings.screen_height))  
         pygame.display.set_caption("Alien Inavasion")
 
-        self.ship = Ship(self.screen)
+        self.ship = Ship(self)
+        self.bullets = pygame.sprite.Group()
 
     def run_game(self):#основной цикл игы
         while True:
             self._check_events()
             self.ship.update()
             self._update_screen()
+            self.bullets.update()
+    
     def _check_events(self):
         #обрабатывается нажатие клавиш
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 sys.exit
             elif event.type == pygame.KEYDOWN:#активирование клавиатуры
-                if event.key == pygame.K_RIGHT:#проверка нажатия
-                    self.ship.moving_right = True  
-                elif event.key == pygame.K_LEFT:#!!!
-                    self.ship.moving_left = True
-
+                self._check_keydown_events(event)
             elif event.type == pygame.KEYUP:
-                if event.key == pygame.K_RIGHT:
-                    self.ship.moving_right = False
-                elif event.key == pygame.K_LEFT:#!!!
-                    self.ship.moving_left = False
+                self._check_keyup_events(event)
+    
+    def _check_keydown_events(self, event):
+        #НАЖАТИЕ КЛАВИШ
+        if event.key == pygame.K_RIGHT:
+            self.ship.moving_right = True
+        elif event.key == pygame.K_LEFT:
+            self.ship.moving_left = True
+        elif event.key == pygame.K_q:
+            sys.exit()
+        elif event.key == pygame.K_SPACE:
+            self._fire_bullet()
+
+    def _check_keyup_events(self, event):
+        #ОТПУСКАНИЕ КЛАВИШ
+        if event.key == pygame.K_RIGHT:
+            self.ship.moving_right = False
+        elif event.key == pygame.K_LEFT:
+            self.ship.moving_left = False
+
+    def _fire_bullet(self):
+        #Создаем новый снаряд
+        new_bullet = Bullet(self)
+        self.bullets.add(new_bullet)
+
 
     def _update_screen(self):
-        #Обновляет изображение на экране
+        #Обновляет изображение на экране и отображает новый
         self.screen.fill(self.settings.bg_color)
         self.ship.blitme()
-        
-        pygame.display.flip()
+        for bullet in self.bullets.sprites():
+            bullet.draw_bullet()        
+            pygame.display.flip()
 
 if __name__ == "__main__":
     ai = AlienInvasion()
     ai.run_game()
-#дорешать !       
+       
 
 
 
